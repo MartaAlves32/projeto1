cc8f94d4f7c7b9c7c31e64a23a2387a376d53a81
Marta Alves <martaalves32@yahoo.com.br>
2025-01-09 19:05:18 -0300

    exercício final - Gulp II

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const {series, parallel} = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
@@ -6,27 +7,34 @@ const uglify = require('gulp-uglify')
 const image = require('gulp-imagemin')
 const stripJs = require('gulp-strip-comments')
 const stripCss = require('gulp-strip-css-comments')
-
+const htmlmin = require('gulp-htmlmin')
+const babel = require('gulp-babel')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 function tarefasCSS(cb) {
 
-    return gulp.src([
+    gulp.src([
             './node_modules/bootstrap/dist/css/bootstrap.css',
             './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             './vendor/owl/css/owl.css',
             './vendor/jquery-ui/jquery-ui.css',
             './src/css/style.css'
         ])
-        .pipe(stripCss())                   // remove comentários
+        .pipe(babel({
+            comments:false,
+            presets:['@babel/env']
+        }))                   
         .pipe(concat('styles.css'))         // mescla arquivos
         .pipe(cssmin())                     // minifica css
         .pipe(rename({ suffix: '.min'}))    // styles.min.css
         .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
 
+    cb()    
 }
 
-function tarefasJS(){
+function tarefasJS(cb){
 
-    return gulp.src([
+    gulp.src([
             './node_modules/jquery/dist/jquery.js',
             './node_modules/bootstrap/dist/js/bootstrap.js',
             './vendor/owl/js/owl.js',
@@ -39,6 +47,9 @@ function tarefasJS(){
         .pipe(uglify())                     // minifica js
         .pipe(rename({ suffix: '.min'}))    // scripts.min.js
         .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
+    
+    cb()    
+    
 }
 
 
@@ -59,6 +70,37 @@ function tarefasImagem(){
         .pipe(gulp.dest('./dist/images'))
 }
 
+// POC - Proof of Concept
+function tarefasHTML(cb){
+
+    gulp.src('./src/**/*.html')
+        .pipe(htmlmin({ collpseWhitespace:true}))
+        .pipe(gulp.dest('./dist'))
+
+    cb()    
+     
+}
+
+gulp.task('serve',function(){
+
+    browserSync.init({
+        server:{
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change',process)
+    gulp.watch('./src/**/*').on('change',reload) 
+})
+
+function end(cb){
+    console.log('tarefas concluidas')
+}
+
+const process = series(tarefasHTML, tarefasJS, tarefasCSS, end)
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
 exports.images = tarefasImagem
+
+
+exports.default = process
